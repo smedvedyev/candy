a04256b696680493a06105bc6bffb6b8afe5a1be
smedvedyev <serhey.medvedyev@gmail.com>
2024-05-07 14:09:52 +0200

    add test cases

---
+++ src/main/java/Candy.java
@@ -0,0 +1,57 @@
+
+/**
+ * k kids together receive c candies.
+ * They wonder whether it is possible
+ * to divide all candies fairly.
+ *
+ * If this is possible, they also want to
+ * know how many candies each of them gets.
+ * This is a number q such that c == k * q .
+ * k, c, and q are non-negative integers, less than 10^18.
+ *
+ * <p>
+ * For instance, divide(3, 15) should return 5, since 3 evenly divides 15,
+ * yielding 5 as quotient.
+ *
+ * NOTE: divide() does not have to be robust; that is, you may assume that the
+ * precondition is always satisfied: {@code 0 <= k < 10^18 and 0 <= c < 10^18}.
+ *
+ * <p>
+ * Apply Test-Driven Development to solve this problem.
+ *
+ * <!--//# BEGIN TODO: Name, student ID, and date-->
+ * <p>
+ * <b>Serhiy Medvedyev, 1948237, 06.05.24</b></p>
+ * <!--//# END TODO-->
+ */
+public class Candy {
+
+    /**
+     * Returns a negative value to indicate that the division is impossible, and
+     * otherwise, returns the number of candies each kid gets.
+     *
+     * @param k number of kids
+     * @param c number of candies
+     * @return a negative value when division is impossible, and otherwise the
+     * number of candies each kid gets
+     * @pre {@code 0 <= k < 10^18 && 0 <= c < 10^18}
+     * @post {@code
+     *   \return < 0 && \not (\exists int q; 0 <= q < 10^18; c == q * k)
+     *   ||
+     *   0 <= \return < 10^18 && c == \return * k}
+     */
+    static long divide(long k, long c) {
+        long result; // value to be returned
+        if (k == 0 || c == 0) { //no kids, no candies case
+            result = -1;
+        } else {
+            if (c % k == 0) {
+                result = c / k;
+            } else {
+                result = -1;
+            }
+        }
+        return result;
+    }
+
+}
---
+++ src/test/java/CandyTest.java
@@ -0,0 +1,84 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+/**
+ * A JUnit test case class.
+ * Every method starting with the word "test" will be called when running
+ * the test with JUnit.
+ *
+<!--//# BEGIN TODO: Name, student ID, and date-->
+<p><b>Medvedyev Serhiy, 1948237, 06.05.24</b></p>
+<!--//# END TODO-->
+ */
+public class CandyTest {
+
+    static final Candy SUT = null; // to simplify method calls
+
+    static final long MAX_VALUE = 999999999999999999L;
+
+    /**
+     * Checks the result of SUT.divide(k, c).
+     */
+    private void check(long k, long c, boolean expected) {
+        System.out.println("divide(" + k + ", " + c + ")");
+        long result = SUT.divide(k, c);
+        System.out.println("  result = " + result);
+        assertEquals(expected, 0 <= result, "possible (0 <= result)");
+        if (0 <= result) {
+            assertTrue(result <= MAX_VALUE, "range (result <= MAX_VALUE)");
+            assertEquals(result * k, c, "quotient (result * k == c)");
+        }
+    }
+
+    // Test cases
+
+    /** Regular input, possible to divide.*/
+    
+    @Test 
+    public void testDivideGivenExample() {
+        check(3, 15, true);
+    }
+    /**Regular input, not possible to divide.*/
+    
+    @Test
+    public void testDivideCannotDivide() {
+        check(3, 20, false);
+    }
+    /**Border value of candies, can be divided.*/
+    
+    @Test
+    public void testDivideBorderCaseTrue() {
+        check(1, MAX_VALUE, true);
+    } 
+    /**Border value of candies, although cannot be divided.*/
+    
+    @Test
+    public void testDivideBorderCaseFalse() {
+        check(MAX_VALUE - 1, MAX_VALUE, false);
+    }
+    /**Border value of children, although cannot be divided.*/
+    
+    @Test
+    public void testDivideBorderChildrenTrue() {
+        check(MAX_VALUE, MAX_VALUE, true);
+    }
+    /**Border value of children, although cannot be divided.*/
+    
+    @Test
+    public void testDivideBorderChildrenFalse() {
+        check(MAX_VALUE, 1, false);
+    }
+    /**No kids, not possible to divide between 0 kids.*/
+    
+    @Test
+    public void testDivideNoKids() {
+        check(0, 1, false);
+    }
+    /**No candies, cannot be divided + division by 0.*/
+    
+    @Test
+    public void testDivideNoCandies() {
+        check(1, 0, false);
+    }
+
+}
